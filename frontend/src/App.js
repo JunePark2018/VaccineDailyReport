f490d03d1674900945f38df86e5a4e3817b44f57
jeonhyunsik <jeonhs9110@gmail.com>
2026-01-02 15:45:21 +0900

    header box organized fit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,21 @@ import './App.css';
 import axios from 'axios';
 import Main from './pages/Main.jsx';
 import Header from './header.js';
+import logo from './logo.png';
+import Searchbar from './Searchbar';
+import Button from './Button';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Main/>
+      <Header
+        leftChild={<img src={logo} alt="Vaccine 일보 Logo" style={{ height: '50px' }} />}
+        midChild={<Searchbar />}
+        rightChild={<Button text={'로그인'} onClick={() => {
+
+        }} />}
+      />
+      <Main />
     </div>
   );
 }
--- frontend/src/header.js
+++ frontend/src/header.js
@@ -1,25 +1,19 @@
 import React from 'react';
 import './header.css'; // We will create this next
-import logo from './logo.png';
-import Searchbar from './Searchbar';
-import Button from './Button';
 
-const Header = () => {
+const Header = ({className = "", leftChild, midChild, rightChild, darkmode}) => {
   return (
-    <header className="header-container">
-       <div className="left-child">
-                {/* 2. Replace the placeholder with the img tag */}
-                <img src={logo} alt="Vaccine 일보 Logo" style={{ height: '50px' }} />
-            </div>
-      
-      <div className="mid-child">
-        <Searchbar/>
+    <header className={"Header " + className}>
+      <div className="left-child">
+        {leftChild}
       </div>
-      
-      <div className="right-child">
-        <Button text={'로그인'} onClick={() => {
 
-      }}/>
+      <div className="mid-child">
+        {midChild}
+      </div>
+
+      <div className="right-child">
+        {rightChild}
       </div>
     </header>
   );
